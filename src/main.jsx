78ff042937c954b451ee3ae2d14e652e3996ae18
aravindpyaram-source <aravindpyaram@gmail.com>
2025-09-09 20:37:26 -0700

    Fix routing for GitHub Pages SPA

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,13 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
-import "./index.css";
-import { BrowserRouter } from "react-router-dom";
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { HashRouter } from 'react-router-dom'
+import App from './App.jsx'
+import './index.css'
 
-const basename = import.meta.env.DEV ? "/" : "/Aravindandco";
-
-ReactDOM.createRoot(document.getElementById("root")).render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename={basename}>
+    <HashRouter>
       <App />
-    </BrowserRouter>
-  </React.StrictMode>
-);
+    </HashRouter>
+  </React.StrictMode>,
+)
