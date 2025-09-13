cfba752d5359fc12563fedb73869e3f4794a06ca
aravindpyaram-source <aravindpyaram@gmail.com>
2025-09-12 17:35:16 -0700

    mainjsx updated

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,13 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
-import "./index.css";
-import { BrowserRouter } from "react-router-dom";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { HashRouter } from 'react-router-dom';
+import App from './App.jsx';
+import './index.css';  // Global styles
 
-const basename = import.meta.env.DEV ? "/" : "/AravindandCompany";
-
-ReactDOM.createRoot(document.getElementById("root")).render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename={basename}>
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
